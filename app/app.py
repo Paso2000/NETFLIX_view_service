eafb4209020b57d27c07344d38d45b8f344699ca
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-01-08 23:40:00 +0100

    add CORS

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ Components:
 from flask import Flask
 from services.db import init_db
 from routes import init_routes  # Import routes to avoid circular dependencies
-
+from flask_cors import CORS
 
 def create_app():
     """
@@ -31,6 +31,8 @@ def create_app():
 
     # Application configuration
     app.config["MONGO_URI"] = "mongodb://view_mongodb:27017/viewdb"
+
+    CORS(app)
 
     # Initialize database and routes
     init_db(app)
